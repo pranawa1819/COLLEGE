0c6280a09477c47fd3342ba24e46e217b0466913
PRANAWA K.C <161146105+pranawa1819@users.noreply.github.com>
2024-02-25 19:02:30 +0545

    Add files via upload

---
+++ fourth sem/OperatingSystem/FIFOReplacement.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main(){
+int incomingStream[] = {7, 0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1};
+int pageFaults = 0;
+int frames = 3;
+int m, n, s, pages;
+pages = sizeof(incomingStream)/sizeof(incomingStream[0]);
+printf("Incoming \t Frame 1 \t Frame 2 \t Frame 3");
+int temp[frames];
+for(m = 0; m < frames; m++){
+temp[m] = -1;
+}
+for(m = 0; m < pages; m++){
+s = 0;
+for(n = 0; n < frames; n++){
+if(incomingStream[m] == temp[n]){
+s++;
+pageFaults--;
+}
+}
+pageFaults++;
+if((pageFaults <= frames) && (s == 0)){
+temp[m] = incomingStream[m];
+}else if(s == 0){
+temp[(pageFaults - 1) % frames] = incomingStream[m];
+}
+printf("\n");
+printf("%d\t\t",incomingStream[m]);
+for(n = 0; n < frames; n++){
+if(temp[n] != -1) printf(" %d\t\t", temp[n]);
+else printf(" - \t\t");
+}
+}
+printf("\nTotal Page Faults:\t%d\n", pageFaults);
+return 0;
+}
