56e6949d7d8a494c8962cf410953e923f53299ce
PRANAWA K.C <161146105+pranawa1819@users.noreply.github.com>
2024-02-26 20:16:34 +0545

    Add files via upload

---
+++ fourth sem/TOC/TuringMachineQuestion4.cpp
@@ -0,0 +1,80 @@
+//1.	WAP to simulate a Turing Machine that accepts language L= {WW^r|n>=1} over alphabet {a,b} using c++.
+
+#include <iostream>
+#include <cstring>
+
+#define MAX_LENGTH 100
+
+using namespace std;
+
+char tape[MAX_LENGTH];
+int tapehead = 1;
+int state = 0;
+
+int action(char input_char, char replace_with, char move, int new_state) {
+    if (tape[tapehead] == input_char) {
+        tape[tapehead] = replace_with;
+        state = new_state;
+        if (move == 'L') {
+            tapehead -= 1;
+        } else {
+            tapehead += 1;
+        }
+        return 1;
+    }
+    return 0;
+}
+
+int main() {
+    char string[MAX_LENGTH];
+    cout << "Enter String: ";
+    cin >> string;
+    int length = strlen(string) + 2;
+    memset(tape, 'B', length);
+    int i = 1;
+    for (int j = 0; j < strlen(string); j++) {
+        tape[i] = string[j];
+        i++;
+    }
+
+    int oldtapehead = -1;
+    int accept = 0;
+    while (oldtapehead != tapehead) {
+        oldtapehead = tapehead;
+        if (state == 0) {
+            if (action('a', 'B', 'R', 1) || action('b', 'B', 'R', 4) || action('B', 'B', 'R', 6)) {
+                continue;
+            }
+        } else if (state == 1) {
+            if (action('a', 'a', 'R', 1) || action('b', 'b', 'R', 1) || action('B', 'B', 'L', 2)) {
+                continue;
+            }
+        } else if (state == 2) {
+            if (action('a', 'B', 'L', 3)) {
+                continue;
+            }
+        } else if (state == 3) {
+            if (action('a', 'a', 'L', 3) || action('b', 'b', 'L', 3) || action('B', 'B', 'R', 0)) {
+                continue;
+            }
+        } else if (state == 4) {
+            if (action('a', 'a', 'R', 4) || action('b', 'b', 'R', 4) || action('B', 'B', 'L', 5)) {
+                continue;
+            }
+        } else if (state == 5) {
+            if (action('b', 'B', 'L', 3)) {
+                continue;
+            }
+        } else {
+            accept = 1;
+        }
+    }
+
+    if (accept) {
+        cout << "The above string is accepted by TM." << endl;
+    } else {
+        cout << "The above string is not accepted by TM." << endl;
+    }
+
+    return 0;
+}
